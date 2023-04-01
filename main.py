4eff27ca1020ef37555c90d022ff4e48a206c769
AfanasevKirill07 <117385505+AfanasevKirill07@users.noreply.github.com>
2023-04-01 13:43:53 +0300

    add support of connect4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from TOKEN import TOKEN
 import logging
 import sqlite3
 import dbapi
+import connect4
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -19,6 +20,13 @@ con.commit()
 
 
 class YLBotClient(discord.Client):
+    def __init__(self, intents):
+        super().__init__(intends)
+        is_game = False
+        game = None
+        p1id = None
+        p2id = None
+        ordr = None
 
     async def on_ready(self):
         logger.info(f'{self.user} has connected to Discord!')
@@ -30,7 +38,7 @@ class YLBotClient(discord.Client):
         async def on_member_join(self, member):
             await member.create_dm()
             await member.dm_channel.send(
-                f'Здравствуйте, {member.name}, как поживаете?'
+                f'Здравствуйте, {member.name}, хотите поиграть в Соедини 4?'
             )
 
     async def on_message(self, message):
@@ -41,10 +49,56 @@ class YLBotClient(discord.Client):
             f"{message.channel} :: {message.author} :: {message.content}"
             f" :: {message.type} :: {message.components} :: {' ;; '.join(message.attachments) if message.attachments else None}"
             f" :: {' ;; '.join(message.embeds) if message.embeds else None}  :: {message.created_at}")
-        if message.content or message.attachments:
-            print(message.jump_url)
-            dbapi.add_message(message)
-
+        if 'да' in message.content.lower() and not is_game:
+            self.is_game = True
+            await message.author.dm_channel.send(
+                f'Хорошо, начнём'
+            )
+            self.game = Game()
+            await message.author.dm_channel.send(
+                f'выбираем колонну для заполнения'
+            )
+            self.game.p1 = self.p1id = message.author
+        elif game:
+            if self.ordr == 1:
+                if message.author == self.p1id:
+                    try:
+                        c = int(message.content.split(','))
+                        self.game.move(c, 't1')
+                        self.game.draw()
+                        if self.game.win():
+                            self.p1id = None
+                            self.p2id = None
+                            self.game = None
+                            self.is_game = None
+                    except ValueError:
+                        await message.author.dm_channel.send(f'Только целые числа')
+                        return
+                else:
+                    await message.author.dm_channel.send(f'ходить за других нельзя')
+                    return
+                self.ordr == 2
+            elif self.ordr == 2:
+                if self.p2id and message.author != self.p1id:
+                    self.game.p2 = self.p2id = message.author
+                elif message.author == self.p1id:
+                    await message.author.dm_channel.send(f'ходить за других нельзя')
+                if message.author == self.p2id:
+                    try:
+                        c = int(message.content.split(','))
+                        self.game.move(c, 't2')
+                        if self.game.win():
+                            self.p1id = None
+                            self.p2id = None
+                            self.game = None
+                            self.is_game = None
+                    except ValueError:
+                        await message.author.dm_channel.send(f'Только целые числа')
+                        return
+                elif message.author == self.p2id:
+                    await message.author.dm_channel.send(f'ходить за других нельзя')
+                    return
+                    
 
 intents = discord.Intents.all()
 intents.members = True
